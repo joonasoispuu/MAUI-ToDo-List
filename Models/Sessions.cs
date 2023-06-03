af856a8973d50803c51c6ab9279d822fd6654f8f
joonasoispuu <oispuu.joonas@gmail.com>
2023-06-03 10:46:36 +0300

    Added Speaker Model

--- Models/Sessions.cs
+++ Models/Sessions.cs
@@ -4,17 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace app_myconference.Models
+namespace app_myconference.Models;
+
+public class Sessions
 {
-    internal class Sessions
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Room { get; set; }
-        public string Description { get; set; }
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
-        public string StartTimeDisplay => $"{Start:t}";
-        public string DayDisplay => $"{Start:MMM} {Start:d}";
-    }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Room { get; set; }
+    public string Description { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public string StartTimeDisplay => $"{Start:t}";
+    public string DayDisplay => $"{Start:MMM} {Start:d}";
 }
---
+++ Models/Speaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_myconference.Models;
+
+public class Speaker
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Title { get; set; }
+    public string Company { get; set; }
+    public string Image { get; set; } = "https://pbs.twimg.com/profile_images/1622390901153693696/VwJrc4QB_400x400.jpg";
+}
